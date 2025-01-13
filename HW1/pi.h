07dcd4a4ec8712a142e2956972d72ef3340acfef
AntoVara <avaragnolo3@gatech.edu>
2025-01-13 18:23:04 -0500

    gitsetup

---
+++ HW1/pi.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctime>
+#include <cstring>
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <mpi.h>
+
+
+double pi_calc(long int n) {
+    
+    // Write your code below
+    ////////////////////////////////////////
+    
+
+
+    ////////////////////////////////////////
+    return 0.0;
+}
